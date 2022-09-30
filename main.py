f3735784b188560842466f7ce5152481b6d3a385
Dablnoy <go.8761@mail.ru>
2022-10-01 01:24:30 +0600

    1 task

---
+++ main.py
@@ -0,0 +1,31 @@
+balance=0
+def deposit(amount):
+    global balance
+    balance += amount
+    return balance
+def withdraw(amount):
+    global balance
+    balance -= amount
+    return balance
+class Money:
+    def __init__(self,amount):
+        self.amount=amount
+money=Money(int(input("How much money do u want to deposite or withdraw: ")))
+while(True):
+    print("1 for add money")
+    print("2 for withdraw")
+    print("3 for exit")
+    a=int(input())
+    if (a==1):
+        print(deposit(money.amount))
+    elif(a==2):
+        if(balance<=100):
+            print("You dont have money")
+        else:
+            print(withdraw(money.amount))
+    elif(a==3):
+        break
+    else:
+        print("Please enter 1 or 2")
+
+
